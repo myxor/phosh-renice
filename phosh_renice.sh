f39574f68c3e02cf3274c53c743e705455603247
Marco H <myxor@users.noreply.github.com>
2020-08-11 15:12:31 +0200

    Fix "PIDS: command not found" error
    Fix "bash /usr/local/sbin/phosh_renice: line 3: PIDS: command not found" error

--- phosh_renice.sh
+++ phosh_renice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PIDS = "$(pidof phoc) $(pidof phosh) $(pidof squeekboard) $(pidof calls)"
+PIDS="$(pidof phoc) $(pidof phosh) $(pidof squeekboard) $(pidof calls)"
 renice -n -1 -p $PIDS
 
